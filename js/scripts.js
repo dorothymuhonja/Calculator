ee888c1fc68d0c39a1d7fe00658a796fa414bf21
dorothymuhonja <dorothymuhonja7@gmail.com>
2020-12-16 12:34:35 +0300

    add jquery to addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 var add = function(number1, number2){
 return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
  
  var subtract = function(number1, number2) {
 return number1 - number2;
@@ -24,3 +22,14 @@ return number1 / number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(divide(number1, number2));
+
+$(document).ready(function () {
+  $('form#add').submit(function (event) {
+    event.preventDefault();
+
+    var number1 = parseInt($('#add1').val());
+    var number2 = parseInt($('#add2').val());
+    var result = add(number1, number2);
+    $('#output').text(result);
+  });
+});
